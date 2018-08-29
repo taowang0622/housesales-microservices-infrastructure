afee1d9dd1d44e06a0c32cc3228a23c0be44ee5a
TaoWang <taowang0622@gmail.com>
2018-08-29 01:04:07 -0400

    Monolithic: create spring boot app and integrate mybatis

---
+++ src/main/java/io/github/taowang0622/homehive/HomehiveApplication.java
@@ -0,0 +1,12 @@
+package io.github.taowang0622.homehive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomehiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomehiveApplication.class, args);
+	}
+}
---
+++ src/test/java/io/github/taowang0622/homehive/HomehiveApplicationTests.java
@@ -0,0 +1,16 @@
+package io.github.taowang0622.homehive;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HomehiveApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
